09f35dc270746220f998d92ef03bd43c6443383e
VelixHalim <55150412+VelixHalim@users.noreply.github.com>
2021-02-11 15:53:20 +0700

    Initial commit

---
+++ app/src/main/java/umn/ac/id/week03_30008/DaftarKataAdapter.java
@@ -0,0 +1,27 @@
+package umn.ac.id.week03_30008;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DaftarKataAdapter extends RecyclerView.Adapter<DaftarKataAdapter.KataViewHolder> {
+
+    @NonNull
+    @Override
+    public DaftarKataAdapter.KataViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DaftarKataAdapter.KataViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    
+}
